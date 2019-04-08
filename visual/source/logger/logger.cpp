e27fdc4a066ff0f4252a9475ef8e053b87d42f5b
mvxxx <michal.tyrolski@gmail.com>
2019-04-08 17:57:14 +0200

    [ITL-1] Added Logger

---
+++ visual/source/logger/logger.cpp
@@ -0,0 +1,87 @@
+#include "logger.hpp"
+
+namespace mv
+{
+    void Logger::Log(const std::string& message, const Logger::STREAM& stream, const Logger::TYPE& type) noexcept
+    {
+        std::string prefix;
+        setPrefix(type,prefix);
+        sendMessage(message, stream,prefix);
+    }
+
+    void Logger::sendMessage(const std::string& message, Logger::STREAM stream, std::string &prefix) noexcept
+    {
+        std::chrono::time_point<std::chrono::system_clock> date = std::chrono::system_clock::now();
+        std::time_t time = std::chrono::system_clock::to_time_t(date);
+
+        switch (stream)
+        {
+            case Logger::STREAM::FILE:
+            {
+                fileMessage(message,prefix,time);
+                break;
+            }
+
+            case Logger::STREAM::CONSOLE:
+            {
+                consoleMessage(message,prefix, time);
+                break;
+            }
+
+            case Logger::STREAM::BOTH:
+            {
+                consoleMessage(message,prefix, time);
+                fileMessage(message,prefix, time);
+                break;
+            }
+        }
+    }
+
+    void Logger::consoleMessage(const std::string& message, std::string &prefix, std::time_t& time) noexcept
+    {
+        std::cout << std::ctime(&time);
+        std::cout << prefix << ' ';
+        std::cout << message << "\n\n";
+    }
+
+    void Logger::fileMessage(const std::string& message, std::string &prefix, std::time_t& time) noexcept
+    {
+        std::ofstream file("data/log/log.txt",std::ios::app);
+
+        file << std::ctime(&time);
+        file << prefix << ' ';
+        file << message << "\n\n";
+    }
+
+    void Logger::setPrefix(Logger::TYPE type, std::string &prefix) noexcept
+    {
+        switch (type)
+        {
+
+            case Logger::TYPE::ERROR:
+            {
+                prefix = "[ERROR]";
+                break;
+            }
+
+
+            case Logger::TYPE::INFO:
+            {
+                prefix = "[INFO]";
+                break;
+            }
+
+            case Logger::TYPE::SUGGESTION:
+            {
+                prefix = "[SUGGESTION]";
+                break;
+            }
+
+            case Logger::TYPE::WARNING:
+            {
+                prefix = "[WARNING]";
+                break;
+            }
+        }
+    }
+}
---
+++ visual/source/logger/logger.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+namespace mv
+{
+    /**
+    * @brief Tool for sending messages
+    */
+    class Logger
+    {
+        /* ===Objects=== */
+    public:
+        /**
+        * @brief type of streaming - sending messages
+        */
+        enum class STREAM
+        {
+            CONSOLE,
+            FILE,
+            BOTH
+        };
+
+        /**
+        * @brief type of information
+        */
+        enum class TYPE
+        {
+            INFO,
+            SUGGESTION ,
+            WARNING,
+            ERROR
+        };
+    protected:
+    private:
+        /* ===Methods=== */
+    public:
+        /**
+        * @brief Method for sending messages
+        * @param message - contents
+        * @param stream - type of stream
+        * @param type - kind of message
+        * @see STREAM
+        * @see TYPE
+        */
+        static void Log(const std::string&,
+                const Logger::STREAM& = Logger::STREAM::CONSOLE,
+                const Logger::TYPE& = Logger::TYPE::ERROR) noexcept;
+    protected:
+    private:
+
+        /**
+        * @brief sends message
+        * @param message - contents
+        * @param stream - type of stream
+        * @param prefix of message
+        * @see STREAM
+        * @see TYPE
+        */
+        static void sendMessage(const std::string& message,
+                Logger::STREAM stream,
+                std::string& prefix) noexcept;
+
+        /**
+        * @brief sends message to console
+        * @param message - contents
+        * @param prefix of message
+        * @param time of message
+        */
+        static void consoleMessage(const std::string&message,
+                std::string& prefix,
+                std::time_t& time) noexcept;
+
+        /**
+        * @brief sends message to file
+        * @param message - contents
+        * @param prefix of message
+        * @param time of message
+        */
+        static void fileMessage(const std::string&,
+                std::string& prefix,
+                std::time_t& time) noexcept;
+
+        /**
+        * @brief sends message to console
+        * @param prefix of message
+        * @param type of message
+        * @see TYPE
+        */
+        static void setPrefix(Logger::TYPE type,
+                std::string& prefix) noexcept;
+    };
+}
