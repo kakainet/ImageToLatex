e143f13f3c78772322738fb91147f08a8591e653
mvxxx <michal.tyrolski@gmail.com>
2019-04-09 22:23:25 +0200

    [ITL-1] Manager added

---
+++ visual/source/manager/manager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include "../logger/logger.hpp"
+#include "../config/config.hpp"
+
+template <class T, class C>
+class Manager
+{
+protected:
+    std::unique_ptr<C> storage;
+public:
+    Manager();
+    virtual bool load_data() = 0;
+    virtual T& get_random_element() = 0;
+};
+
+template<class T, class C>
+Manager<T, C>::Manager()
+{
+    this->storage = std::make_unique<C>();
+}
