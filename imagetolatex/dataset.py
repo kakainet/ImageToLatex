fa235f942c9d7bbb166bccb0d94787a88c9c81bd
kubachrabanski <kuba.chrabanski@gmail.com>
2019-09-29 03:01:31 +0200

    [ITL-31] added NPSequence and FSSequence.as_np, paths are loaded as list instead of dict

--- imagetolatex/dataset.py
+++ imagetolatex/dataset.py
@@ -1,15 +1,12 @@
 from __future__ import division, print_function
 
 import multiprocessing.pool
-import multiprocessing
+import resource
 import sys
 import os
 import os.path
 import re
-import operator
-import functools
 import itertools
-import collections
 import logging
 
 from keras.utils import Sequence
@@ -17,16 +14,23 @@ from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 
+__all__ = ['FSSequence', 'NPSequence']
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 logger.setLevel(logging.DEBUG) 
+
+
+def _memory_usage():
+    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (1024 * 1024)
 
 def _parse_index(path):
     _, name = os.path.split(path)
     return int(re.search(r'[0-9]+', name).group())
 
 def _list_files(path):
-    return [os.path.join(path, x)
+    return [
+        os.path.join(path, x)
         for x in filter(lambda x : not x.startswith('.'), os.listdir(path))
         if os.path.isfile(os.path.join(path, x))
     ]
@@ -38,113 +42,167 @@ def _load_lines(path):
 
 class FSSequence(Sequence):
 
-    def __init__(self, label_paths, feature_paths, batch_size, target_size, batch_indexes=None, thread_count=None, **kwargs):
+    def __init__(self, label_paths, feature_paths, batch_size, variants_count, target_size,
+                label_dtype='U128', feature_dtype='float32', thread_count=None, **kwargs):
+
         self._label_paths = label_paths
         self._feature_paths = feature_paths
 
         self._batch_size = batch_size
+        self._variants_count = variants_count
         self._target_size = target_size
+
+        self._label_dtype = label_dtype
+        self._feature_dtype = feature_dtype
 
         self._thread_count = thread_count
         self._kwargs = kwargs
 
-        if batch_indexes is None:
-            batch_indexes = list(label_paths.keys())
-        self._batch_indexes = batch_indexes
-
     @classmethod
-    def of_path(cls, path, batch_size, target_size, thread_count=None, **kwargs):
-        label_paths = {
-            _parse_index(x) : x for x in _list_files(os.path.join(path, 'labels'))
-        }
+    def of_path(cls, path, batch_size, variants_count, target_size,
+                label_dtype='U128', feature_dtype='float32', thread_count=None, **kwargs):
+
+        label_paths = sorted(
+            _list_files(os.path.join(path, 'labels')), key=_parse_index
+        )
         
-        feature_paths = {
-            i : [[] for x in range(batch_size)]
-            for i in range(len(label_paths))
-        }
+        feature_paths = sorted(
+            _list_files(os.path.join(path, 'features')), key=_parse_index
+        )
 
-        for feature_path in _list_files(os.path.join(path, 'features')):
-            feature_index = _parse_index(feature_path)
-            feature_paths[feature_index // batch_size][feature_index % batch_size].append(feature_path)
+        features_per_batch = batch_size * variants_count
 
-        return cls(label_paths, feature_paths, batch_size, target_size, thread_count, **kwargs)
+        feature_paths = [
+            feature_paths[i : i + features_per_batch]
+            for i in range(0, len(feature_paths), features_per_batch)
+        ]
+
+        return cls(
+            label_paths, feature_paths,
+            batch_size, variants_count, target_size,
+            label_dtype, feature_dtype, thread_count,
+            **kwargs
+        )
 
     @property
     def batch_size(self):
         return self._batch_size
 
     @property
+    def variants_count(self):
+        return self._variants_count
+
+    @property
     def target_size(self):
         return self._target_size
 
-    @property
-    def batch_indexes(self):
-        return self._batch_indexes.copy()
-
     def subset(self, iterable):
-        label_paths = {
-            i : self._label_paths[i] for i in iterable
-        }
+        iterable = sorted(iterable)
 
-        feature_paths = {
-            i : self._feature_paths[i] for i in iterable
-        }
+        label_paths = [self._label_paths[i] for i in iterable]
+        feature_paths = [self._feature_paths[i] for i in iterable]
 
         return FSSequence(
             label_paths, feature_paths,
-            self._batch_size, self._target_size, sorted(iterable),
-            self._thread_count, **self._kwargs
+            self._batch_size, self._variants_count, self._target_size,
+            self._label_dtype, self._feature_dtype, self._thread_count,
+            **self._kwargs
         )
-    
-    def load_batch(self, idx):
+
+    def load_batch(self, idx, size_limit=None):
         unedited_labels = _load_lines(self._label_paths[idx])
         feature_paths = self._feature_paths[idx]
 
+        if size_limit is not None:
+            feature_paths = feature_paths[:size_limit * self._variants_count]
+
         features = self.load_features(
-            functools.reduce(operator.concat, feature_paths), self._target_size,
-            thread_count=self._thread_count, **self._kwargs
+            feature_paths, self._target_size, self._feature_dtype, self._thread_count,
+            **self._kwargs
         )
 
-        labels = np.array(list(
-            itertools.chain.from_iterable(
-                (itertools.repeat(unedited_labels[i], len(x)) for i, x in enumerate(feature_paths))
-            )
-        ))
+        labels = np.fromiter(
+            itertools.chain.from_iterable(itertools.repeat(x, self._variants_count) for x in unedited_labels),
+            dtype=self._label_dtype, count=len(unedited_labels) * self._variants_count
+        )
 
         return features, labels
 
     def __getitem__(self, idx):
-        return self.load_batch(self._batch_indexes[idx])
+        return self.load_batch(idx)
 
     def __len__(self):
         return len(self._label_paths)
 
     @staticmethod
-    def load_feature(path, **kwargs):
-        return img_to_array(
-            load_img(path, **kwargs)
-        )
+    def load_feature(path, target_size=None, **kwargs):
+        return img_to_array(load_img(path, target_size=target_size, **kwargs))
 
     @staticmethod
-    def load_features(paths, target_size, thread_count=None, **kwargs):
-        features = np.empty((len(paths), *target_size), dtype='float32')
-        lock = multiprocessing.Lock()
+    def load_features(paths, target_size, feature_dtype='float32', thread_count=None, **kwargs):
+        features = np.empty((len(paths), *target_size), dtype=feature_dtype)
 
-        def load(index, path):
-            feature = FSSequence.load_feature(path, target_size=target_size, **kwargs)
-            with lock:
-                features[index, :, :, :] = feature
+        logger.debug('MU %d (+-) %d', _memory_usage(), features.nbytes / (1024 * 1024))
 
         with multiprocessing.pool.ThreadPool(thread_count) as pool:
-            for i, _ in enumerate(pool.imap_unordered(lambda pair : load(pair[0], pair[1]), enumerate(paths)), 1):
-                sys.stdout.write('\r{0:.2%}'.format(i / len(paths)))
+            load_function = lambda pair : FSSequence.load_feature(pair[1], target_size, **kwargs)
+
+            for index, feature in enumerate(pool.imap_unordered(load_function, enumerate(paths))):
+                sys.stdout.write('\r{0:.2%}'.format((index + 1) / len(paths)))
+                features[index, :, :, :] = feature
             sys.stdout.write('\n')
+
+        logger.debug('MU %d', _memory_usage())
 
         features /= 255
         return features
     
-    def as_np(self, path):
-        raise NotImplementedError 
+    def as_np(self, path, batch_name='batch'):
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        batch_path = os.path.join(path, batch_name)
+
+        for index, (features, labels) in enumerate(self):
+            np.savez_compressed(batch_path + str(index), features=features, labels=labels)
+            sys.stdout.write('{0:.2%}'.format((index + 1) / len(self)))
+        sys.stdout.write('\n')
+
+        return NPSequence.of_path(path)
+
+
+class NPSequence(Sequence):
+
+    def __init__(self, batch_paths):
+        self._batch_paths = batch_paths
+
+    @classmethod
+    def of_path(cls, path):
+        batch_paths = sorted(
+            _list_files(path), key=_parse_index
+        )
+
+        return cls(batch_paths)
+
+    def subset(self, iterable):
+        iterable = sorted(iterable)
+
+        return NPSequence(
+            [self._batch_paths[i] for i in iterable]
+        )
+
+    def load_batch(self, idx):
+        with np.load(self._batch_paths[idx], allow_pickle=True) as ifs:
+            return ifs['features'], ifs['labels']
+
+    def __getitem__(self, idx):
+        return self.load_batch(idx)
+
+    def __len__(self):
+        return len(self._batch_paths)
+
+    def as_fs(self):
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
