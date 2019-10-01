3cf5bfe0d8143ba23dd8aadadc3a0c4a1fcbd745
mvxxx <michal.tyrolski@gmail.com>
2019-10-01 21:23:52 +0200

    [ITL-46] bash workers

---
+++ tools/set/set.sh
@@ -0,0 +1,24 @@
+for var in "$@"
+do
+    mkdir -p "${var}_aux"
+    cp transform.asy "${var}_aux/"
+    mv $var "${var}_aux/"
+    cd "${var}_aux"
+    mv $var "raw_tex.in"
+    asy transform -noV -render 5 -f png &
+    cd ..
+done
+
+wait
+
+for var in "$@"
+do
+    cd "${var}_aux"
+    mv "raw_tex.in" $var
+    wait
+    mv $var ../
+    rm -f transform.asy
+    cd ..
+done
+
+wait
