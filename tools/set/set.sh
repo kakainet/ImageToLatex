a9d0540991f377e90a7bf767506796773b366dcc
mvxxx <michal.tyrolski@gmail.com>
2019-10-15 02:52:04 +0200

    [ITL-46] Updated script

--- tools/set/set.sh
+++ tools/set/set.sh
@@ -2,15 +2,15 @@ for var in "$@"
 do
     mkdir -p "${var}_aux"
     cp transform.asy "${var}_aux/"
-    mv $var "${var}_aux/"
+    cp $var "${var}_aux/"
     cd "${var}_aux"
     mv $var "raw_tex.in"
-    asy transform -noV -render 5 -f png &
+    asy transform -noV -render 3 -f png &
     cd ..
 done
 mkdir -p output
 wait
-idx=0
+idx=1
 for var in "$@"
 do
     cd "${var}_aux"
@@ -18,12 +18,15 @@ do
     wait
     mv $var ../
     rm -f transform.asy
-    for file in *.png
-    do
-        mv "$file" "sample${idx}.png"
-        mv "sample${idx}.png" ../output/
-        ((++idx))
-    done
+
+    for file in `ls *.png | sort -V`;
+    do 
+        mv "$file" "eq${idx}.png"
+        wait
+        mv "eq${idx}.png" ../output/
+        ((++idx)) 
+    done;
+
     cd ..
     wait
     rm -rf "${var}_aux"
