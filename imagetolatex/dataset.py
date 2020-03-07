e7efcfe4fd2d13a07a8047bd65df0f391b32bc9b
kebabdubai <kebabdubai@protonmail.com>
2020-03-07 15:07:23 +0100

    [ITL-63] Fixed loop indexes in load_layered, changed _split_lines

--- imagetolatex/dataset.py
+++ imagetolatex/dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from .sequence import LayeredSequence
 
+
 def _list_files(path):
     return [
         os.path.join(path, x)
@@ -18,25 +19,29 @@ def _load_lines(path):
         return [x.strip() for x in ifs.readlines()]
 
 def _split_line(line, length):
+    # TODO reconsider, maybe could be solved in loop range
     parts = line.split('\t')
-    return parts + [None] * (length - len(parts))
+    return parts[:length] if len(parts) > length else parts + [None] * (length - len(parts))
 
 def load_layered(input_path, category_encoder, layer_count,
-                 feature_shape=(32, 32, 1), batch_size=1024, thread_count=None, **feature_kwargs):
+                 feature_shape=(32, 32, 3), batch_size=100, thread_count=None, **feature_kwargs):
 
     ungrouped_feature_paths = _list_files(os.path.join(input_path, 'features'))
     grouped_feature_paths = collections.defaultdict(list)
 
     for feature_path in ungrouped_feature_paths:
         feature_index, *_ = re.findall(r'[0-9]+', feature_path)
-        grouped_feature_paths[feature_index].append(feature_path)
+        grouped_feature_paths[int(feature_index)].append(feature_path)
 
     feature_paths = []
 
-    for feature_index in range(len(ungrouped_feature_paths)):
+    for feature_index in range(len(grouped_feature_paths)):
         feature_paths.extend(grouped_feature_paths[feature_index])
 
     unparsed_labels = _load_lines(os.path.join(input_path, 'labels.txt'))
+
+    assert len(unparsed_labels) == len(grouped_feature_paths)
+
     label_layers = np.empty(
         (layer_count, len(ungrouped_feature_paths), len(category_encoder)),
         dtype='float32'
@@ -47,7 +52,8 @@ def load_layered(input_path, category_encoder, layer_count,
             encoded_label_part = category_encoder.encode(label_part)
 
             for feature_variant_index in range(len(grouped_feature_paths[label_line_index])):
-                label_index = label_line_index * len(unparsed_labels) + feature_variant_index
+                # TODO fix, may break on uneven variant counts
+                label_index = label_line_index * len(grouped_feature_paths[label_line_index]) + feature_variant_index
                 label_layers[label_layer_index, label_index, :] = encoded_label_part
 
     return LayeredSequence(
