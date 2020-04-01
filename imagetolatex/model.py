1d1909f58b1d3ecad6ef333c3180125294bf95d0
kebabdubai <kebabdubai@protonmail.com>
2020-04-01 22:51:57 +0200

    [ITL-63] Adapted LayeredModel to LayeredSequence

--- imagetolatex/model.py
+++ imagetolatex/model.py
@@ -1,29 +1,33 @@
-import os
+from random import randrange
 import os.path
-import logging
-from datetime import datetime
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.utils import to_categorical
 import keras.losses
 import keras.optimizers
 
-
+# TODO add callbacks, progress handlers, add save to .h5l, add predict, basic interface
 class LayeredModel:
-
-    def __init__(self, input_shape, category_encoder, supported_characters):
-
-        self.get_layer_model = self.get_complex_equation_model
-
+    
+    def __init__(self, input_shape, layer_count, category_encoder, layer_init):
         self._layer_models = [
-            self.get_layer_model(input_shape, len(category_encoder))
-            for _ in range(supported_characters)
+            layer_init(input_shape, len(category_encoder))
+            for _ in range(layer_count)
         ]
 
-    def fit_on_flat(self, train_sequences, test_sequences, epochs, verbose, save_weights=False):
-        print(self._layer_models)
+    # TODO allow for uneven train/test sets
+    def fit_on_layered(self, train_sequences, test_sequences, epochs):
+        for _ in range(epochs):
+            train_index, test_index = randrange(len(train_sequences)), randrange(len(test_sequences))
+            train_features, train_label_layers = train_sequences[train_index]
+            test_features, test_label_layers = test_sequences[test_index]
+
+            for (train_labels, test_labels), layer_model in zip(zip(train_label_layers, test_label_layers), self._layer_models):
+                layer_model.train_on_batch(x=train_features, y=train_labels)
+                layer_model.test_on_batch(x=test_features, y=test_labels)
+
+        """
         for layer_index, layer_model in enumerate(self._layer_models):
             print('Fitting {0}/{1} layer now'.format(layer_index + 1, len(self._layer_models)))
             layer_model.fit_generator(
@@ -31,50 +35,41 @@ class LayeredModel:
                 validation_data=test_sequences[layer_index],
                 epochs=epochs,
                 verbose=verbose,
-                workers=12
+                **kwargs
             )
+        """
 
-        if save_weights:
-            save_path = 'params/weight_{0}_{1}.ckpt'
-            for layer_index, layer_model in enumerate(self._layer_models):
-                layer_model.save_weights(save_path.format(layer_index, datetime.now().strftime('%Y-%m-%d-%H:%M:%S')))
+def complex_equation_layer(input_shape, num_classes, verbose=False):
+    model = Sequential()
 
-    @staticmethod
-    def get_complex_equation_model(input_shape, num_classes):
-        model = Sequential()
+    model.add(Conv2D(64, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(256, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
 
-        model.add(Conv2D(64, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(128, (5, 5), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(256, (5, 5), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-        model.add(Flatten())
-        model.add(Dense(512, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(num_classes, activation='softmax'))
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adam(),
+                  metrics=['accuracy'])
 
-        model.compile(loss=keras.losses.categorical_crossentropy,
-                      optimizer=keras.optimizers.Adam(),
-                      metrics=['accuracy'])
-
+    if verbose:
         model.summary()
 
-        return model
+    return model
+
 
 
 if __name__ == '__main__':
-    print('test')
-    from keras import backend as K
-
-    K.tensorflow_backend._get_available_gpus()
-    print('test')
-    from encoding import CategoryEncoder
-    from sequence import load_flat
     from string import digits
+    from .encoding import CategoryEncoder
+    from .dataset import load_layered
 
-    print(*digits)
     latex_encoder = CategoryEncoder()
     latex_encoder.extend([
         '+', '-', '^', '{', '}',
@@ -82,21 +77,23 @@ if __name__ == '__main__':
         *digits
     ])
 
-    sequences = load_flat(
+    input_shape = (128, 128, 1)
+    layer_count = 64
+
+    sequence = load_layered(
         os.path.join(os.getcwd(), 'dataset'),
         latex_encoder,
-        len(latex_encoder),
-        (64, 64, 1),
-        50,
+        layer_count,
+        input_shape,
+        batch_size=1000,
         color_mode='grayscale'
     )
-    print('seq size', len(sequences))
-    train_size = int(0.8 * len(sequences))
-    model = LayeredModel((64, 64, 1), latex_encoder, 7)
-    model.fit_on_flat(
-        sequences,
-        sequences, # validation set will be extracted in 0.1.1
-        epochs=10,
-        verbose=1,
-        save_weights=True
+
+    train_sequence, test_sequence = sequence.split(0.5)
+
+    model = LayeredModel(input_shape, layer_count, latex_encoder, complex_equation_layer)
+    model.fit_on_layered(
+        train_sequence,
+        test_sequence,
+        epochs=10000
     )
